d2c19bd9d9291a59dcf01ffcbbaa1efc6ecc8769
Matt Crupe <mcrupe@gmail.com>
2023-01-05 19:20:08 -0600

    added some more options

--- nvim/lua/croopadoop/options.lua
+++ nvim/lua/croopadoop/options.lua
@@ -1,8 +1,23 @@
--- :help options if you want to see what an option does
+-- File settings
 vim.o.backup = false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.swapfile = false
 vim.o.clipboard = "unnamedplus"
+
+-- Text editing settings
 vim.o.smartcase = true
-vim.o.shiftwidth = 4
+vim.o.shiftwidth = 0 -- Defer to tabstop value
 vim.o.tabstop = 4
-vim.o.relativenumber = true 
 vim.o.wrap = false
+
+-- Line number settings
+-- Change numberwith to 6 for better readability when there are hundreds of lines of code
+vim.o.number = true
+vim.o.numberwidth = 6
+vim.o.relativenumber = true 
+
+-- Search case settings
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.jumpoptions = true
