4b600b3d0057ba416e219ce955434c377de456ce
Matt Crupe <mcrupe@gmail.com>
2023-01-11 17:36:49 -0600

    edited keymaps

--- nvim/lua/croopadoop/keymaps.lua
+++ nvim/lua/croopadoop/keymaps.lua
@@ -60,3 +60,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 -- Recenter when location browsing
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>bn", "<cmd>bn<CR>")
